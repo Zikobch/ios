5f503e89378e2e526c978cc2d1c628ef134d99de
Zikobch <129260075+Zikobch@users.noreply.github.com>
2025-05-26 21:27:44 +0100

    Add files via upload

---
+++ Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/macbookpro2021/development/flutter_3_6"
+export "FLUTTER_APPLICATION_PATH=/Users/macbookpro2021/Library/Mobile Documents/com~apple~CloudDocs/Desktop/projectcollab-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/macbookpro2021/Library/Mobile Documents/com~apple~CloudDocs/Desktop/projectcollab-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=15"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/macbookpro2021/Library/Mobile Documents/com~apple~CloudDocs/Desktop/projectcollab-master/.dart_tool/package_config.json"
